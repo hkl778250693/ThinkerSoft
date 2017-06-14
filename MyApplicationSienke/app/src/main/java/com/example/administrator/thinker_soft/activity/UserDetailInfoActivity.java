75bf72bc68deefdcb04d43d688e679ece2c5fb7e
Ranhaihan <594576783@qq.com>
2017-06-14 17:44:39 +0800

    6.14

--- MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/activity/UserDetailInfoActivity.java
+++ MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/activity/UserDetailInfoActivity.java
@@ -217,8 +217,9 @@ public class UserDetailInfoActivity extends AppCompatActivity {
 
     /**
      * EditText竖直方向是否可以滚动
-     * @param editText  需要判断的EditText
-     * @return  true：可以滚动   false：不可以滚动
+     *
+     * @param editText 需要判断的EditText
+     * @return true：可以滚动   false：不可以滚动
      */
     private boolean canVerticalScroll(EditText editText) {
         //滚动的距离
@@ -226,11 +227,11 @@ public class UserDetailInfoActivity extends AppCompatActivity {
         //控件内容的总高度
         int scrollRange = editText.getLayout().getHeight();
         //控件实际显示的高度
-        int scrollExtent = editText.getHeight() - editText.getCompoundPaddingTop() -editText.getCompoundPaddingBottom();
+        int scrollExtent = editText.getHeight() - editText.getCompoundPaddingTop() - editText.getCompoundPaddingBottom();
         //控件内容总高度与实际显示高度的差值
         int scrollDifference = scrollRange - scrollExtent;
 
-        if(scrollDifference == 0) {
+        if (scrollDifference == 0) {
             return false;
         }
         return (scrollY > 0) || (scrollY < scrollDifference - 1);
--- MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/mobile_business/BusinessSendEmailActivity.java
+++ MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/mobile_business/BusinessSendEmailActivity.java
@@ -1,7 +1,6 @@
 package com.example.administrator.thinker_soft.mobile_business;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,48 +9,39 @@ import android.widget.RelativeLayout;
 import com.example.administrator.thinker_soft.R;
 
 /**
- * Created by Administrator on 2017/6/13.
+ * Created by Administrator on 2017/6/14.
  */
-public class BusinessEmailActivity extends Activity {
+public class BusinessSendEmailActivity extends Activity {
     private ImageView back;
-    private RelativeLayout inbox, send;
-
+    private RelativeLayout send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_business_email);
+        setContentView(R.layout.activity_business_send_email);
 
-        bindView();//绑定控件
-        setOnClickListener();//点击事件
+        bindView();
+        setViewClickListener();
     }
 
-    public void bindView() {
+    public void bindView(){
         back = (ImageView) findViewById(R.id.back);
-        inbox = (RelativeLayout) findViewById(R.id.inbox);
         send = (RelativeLayout) findViewById(R.id.send);
     }
 
-    public void setOnClickListener() {
+    public void setViewClickListener(){
         back.setOnClickListener(clickListener);
-        inbox.setOnClickListener(clickListener);
         send.setOnClickListener(clickListener);
     }
 
     View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            switch (v.getId()) {
+            switch (v.getId()){
                 case R.id.back:
                     finish();
                     break;
-                case R.id.inbox:
-                    Intent intent = new Intent(BusinessEmailActivity.this, BusinessEmailInfoActivity.class);
-                    startActivity(intent);
-                    break;
                 case R.id.send:
-                    Intent intent1 = new Intent(BusinessEmailActivity.this, BusinessSendEmailActivity.class);
-                    startActivity(intent1);
                     break;
             }
         }

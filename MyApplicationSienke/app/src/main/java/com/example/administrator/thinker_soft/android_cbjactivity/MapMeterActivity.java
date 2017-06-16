19c66798ad3bdebd5090ff1ca60b5ba630d33d32
hkl778250693 <778250693@qq.com>
2017-06-16 17:03:13 +0800

    dg

--- MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/android_cbjactivity/MapMeterActivity.java
+++ MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/android_cbjactivity/MapMeterActivity.java
@@ -2,14 +2,31 @@ package com.example.administrator.thinker_soft.android_cbjactivity;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
-import com.baidu.mapapi.map.BaiduMapOptions;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 import com.example.administrator.thinker_soft.R;
 
@@ -18,10 +35,30 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MapMeterActivity extends Activity {
+public class MapMeterActivity extends Activity implements SensorEventListener {
+    // 定位相关
+    LocationClient mLocClient;
+    public MyLocationListenner myListener = new MyLocationListenner();
+    private MyLocationConfiguration.LocationMode mCurrentMode;
+    BitmapDescriptor mCurrentMarker;
+    private static final int accuracyCircleFillColor = 0xAAFFFF88;
+    private static final int accuracyCircleStrokeColor = 0xAA00FF00;
+    private SensorManager mSensorManager;
+    private Double lastX = 0.0;
+    private int mCurrentDirection = 0;
+    private double mCurrentLat = 0.0;
+    private double mCurrentLon = 0.0;
+    private float mCurrentAccracy;
+    boolean isFirstLoc = true; // 是否首次定位
+    private MyLocationData locData;
+    private TextView location;
+
     private MapView mMapView;
+    private BaiduMap mBaiduMap;
     private RadioButton openRb;
     private RadioButton closeRb;
+    private RadioButton normalType,satelliteType;
+    private CheckBox trafficType,heatType;
     private boolean mEnableCustomStyle = true;
     //用于设置个性化地图的样式文件
     // 提供三种样式模板："custom_config_blue.txt"，"custom_config_dark.txt"，"custom_config_midnightblue.txt"
@@ -30,6 +67,10 @@ public class MapMeterActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());// 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
+        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
+        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
+        SDKInitializer.setCoordType(CoordType.BD09LL);
         setContentView(R.layout.activity_map_meter);
 
         bindView();
@@ -39,37 +80,103 @@ public class MapMeterActivity extends Activity {
 
     //绑定控件
     private void bindView() {
+        mMapView = (MapView) findViewById(R.id.map_meter);
         openRb = (RadioButton) findViewById(R.id.open_rb);
         closeRb = (RadioButton) findViewById(R.id.close_rb);
+        normalType = (RadioButton) findViewById(R.id.normal_type);
+        satelliteType = (RadioButton) findViewById(R.id.satellite_type);
+        trafficType = (CheckBox) findViewById(R.id.traffic_type);
+        heatType = (CheckBox) findViewById(R.id.heat_type);
+        location = (TextView) findViewById(R.id.location);
     }
 
     //初始化设置
     private void defaultSetting() {
-        MapStatus.Builder builder = new MapStatus.Builder();
-        LatLng center = new LatLng(29.595559,106.579227);  //默认重庆星耀天地29.595559,106.579227
-        float zoom = 11.0f; // 默认 11级
-        Intent intent = getIntent();
-        if (null != intent) {
-            mEnableCustomStyle = intent.getBooleanExtra("customStyle", true);
-            center = new LatLng(intent.getDoubleExtra("y", 29.595559), intent.getDoubleExtra("x", 106.579227));
-            zoom = intent.getFloatExtra("level", 11.0f);
-        }
-        builder.target(center).zoom(zoom);
+        openRb.setChecked(true);
+        normalType.setChecked(true);
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
         setMapCustomFile(MapMeterActivity.this,PATH);
-        mMapView = new MapView(MapMeterActivity.this,new BaiduMapOptions());
-        if (mEnableCustomStyle) {
-            openRb.setChecked(true);
-        } else {
-            closeRb.setChecked(true);
-        }
         MapView.setMapCustomEnable(true);
     }
 
     //点击事件
     public void setViewClickListener() {
-        mMapView = (MapView) findViewById(R.id.map_meter);
         openRb.setOnClickListener(clickListener);
         closeRb.setOnClickListener(clickListener);
+        normalType.setOnClickListener(clickListener);
+        satelliteType.setOnClickListener(clickListener);
+        trafficType.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //开启交通图
+                    mBaiduMap.setTrafficEnabled(true);
+                }else {
+                    //关闭交通图
+                    mBaiduMap.setTrafficEnabled(false);
+                }
+            }
+        });
+        heatType.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //开启热力图
+                    mBaiduMap.setBaiduHeatMapEnabled(true);
+                }else {
+                    //关闭热力图
+                    mBaiduMap.setBaiduHeatMapEnabled(false);
+                }
+            }
+        });
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);//获取传感器管理服务
+        mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;
+        location.setText("普通");
+        View.OnClickListener btnClickListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                switch (mCurrentMode) {
+                    case NORMAL:
+                        location.setText("跟随");
+                        mCurrentMode = MyLocationConfiguration.LocationMode.FOLLOWING;
+                        mBaiduMap.setMyLocationConfiguration(new MyLocationConfiguration(
+                                mCurrentMode, true, mCurrentMarker));
+                        MapStatus.Builder builder = new MapStatus.Builder();
+                        builder.overlook(0);
+                        mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+                        break;
+                    case COMPASS:
+                        location.setText("普通");
+                        mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;
+                        mBaiduMap.setMyLocationConfiguration(new MyLocationConfiguration(
+                                mCurrentMode, true, mCurrentMarker));
+                        MapStatus.Builder builder1 = new MapStatus.Builder();
+                        builder1.overlook(0);
+                        mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder1.build()));
+                        break;
+                    case FOLLOWING:
+                        location.setText("罗盘");
+                        mCurrentMode = MyLocationConfiguration.LocationMode.COMPASS;
+                        mBaiduMap.setMyLocationConfiguration(new MyLocationConfiguration(
+                                mCurrentMode, true, mCurrentMarker));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+        location.setOnClickListener(btnClickListener);
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        // 定位初始化
+        mLocClient = new LocationClient(this);
+        mLocClient.registerLocationListener(myListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+        mLocClient.setLocOption(option);
+        mLocClient.start();
     }
 
     View.OnClickListener clickListener = new View.OnClickListener() {
@@ -78,9 +185,21 @@ public class MapMeterActivity extends Activity {
             switch (v.getId()){
                 case R.id.open_rb:
                     MapView.setMapCustomEnable(true);
+                    Log.i("MapMeterActivity","开启个性");
                     break;
                 case R.id.close_rb:
                     MapView.setMapCustomEnable(false);
+                    Log.i("MapMeterActivity","关闭个性");
+                    break;
+                case R.id.normal_type:
+                    //普通地图
+                    mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                    Log.i("MapMeterActivity","普通地图显示了");
+                    break;
+                case R.id.satellite_type:
+                    //卫星地图
+                    mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                    Log.i("MapMeterActivity","卫星地图显示了");
                     break;
             }
         }
@@ -123,25 +242,97 @@ public class MapMeterActivity extends Activity {
     }
 
     @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        double x = sensorEvent.values[SensorManager.DATA_X];
+        if (Math.abs(x - lastX) > 1.0) {
+            mCurrentDirection = (int) x;
+            locData = new MyLocationData.Builder()
+                    .accuracy(mCurrentAccracy)
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(mCurrentDirection).latitude(mCurrentLat)
+                    .longitude(mCurrentLon).build();
+            mBaiduMap.setMyLocationData(locData);
+        }
+        lastX = x;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    /**
+     * 定位SDK监听函数
+     */
+    public class MyLocationListenner implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // map view 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            mCurrentLat = location.getLatitude();
+            mCurrentLon = location.getLongitude();
+            mCurrentAccracy = location.getRadius();
+            locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(mCurrentDirection).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
+
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+
+        }
+
+        public void onReceivePoi(BDLocation poiLocation) {
+        }
+    }
+
+    @Override
     protected void onPause() {
-        super.onPause();
         // activity 暂停时同时暂停地图控件
         mMapView.onPause();
+        super.onPause();
     }
 
     @Override
     protected void onResume() {
-        super.onResume();
         // activity 恢复时同时恢复地图控件
         mMapView.onResume();
+        super.onResume();
+        //为系统的方向传感器注册监听器
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onStop() {
+        //取消注册传感器监听
+        mSensorManager.unregisterListener(this);
+        super.onStop();
     }
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
         // activity 销毁时同时销毁地图控件
         MapView.setMapCustomEnable(false);
+        // 退出时销毁定位
+        mLocClient.stop();
+        // 关闭定位图层
+        mBaiduMap.setMyLocationEnabled(false);
         mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
     }
 
 }

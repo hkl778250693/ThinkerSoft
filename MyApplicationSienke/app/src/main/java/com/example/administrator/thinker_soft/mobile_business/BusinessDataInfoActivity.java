7a947c84dc73f083cda304cd0da7bea68b6fdad9
hkl778250693 <778250693@qq.com>
2017-06-30 15:56:58 +0800

    13122222222

--- MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/mobile_business/BusinessDataInfoActivity.java
+++ MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/mobile_business/BusinessDataInfoActivity.java
@@ -9,12 +9,20 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.administrator.thinker_soft.R;
@@ -41,6 +49,17 @@ public class BusinessDataInfoActivity extends Activity {
     private int res, current_res;
     private SharedPreferences sharedPreferences_login;
     private SQLiteDatabase db;  //数据库
+    private LayoutInflater inflater;  //转换器
+    private View view;
+    private PopupWindow popupWindow;
+    private LinearLayout dateLayout,timeLayout,rootLinearlayout;
+    private RadioButton cancelRb,saveRb;
+    private DatePicker date;
+    private TimePicker time;
+    private boolean dateChanged = false;
+    private boolean timeChanged = false;
+    private String dataString, timeString;
+    private int year, month, day, hour, minutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +71,8 @@ public class BusinessDataInfoActivity extends Activity {
         setOnClickListener();//点击事件
     }
 
-    public void bindView() {
+    //绑定控件
+    private void bindView() {
         back = (ImageView) findViewById(R.id.back);
         title = (EditText) findViewById(R.id.title);
         customerName = (EditText) findViewById(R.id.customer_name);
@@ -62,6 +82,7 @@ public class BusinessDataInfoActivity extends Activity {
         startDate = (TextView) findViewById(R.id.start_date);
         endDate = (TextView) findViewById(R.id.end_date);
         saveBtn = (Button) findViewById(R.id.save_btn);
+        rootLinearlayout = (LinearLayout) findViewById(R.id.root_linearlayout);
     }
 
     //初始化设置
@@ -71,12 +92,18 @@ public class BusinessDataInfoActivity extends Activity {
         sharedPreferences_login = getSharedPreferences("login_info", Context.MODE_PRIVATE);
         c = Calendar.getInstance();
         dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        dateFormat1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        dateFormat1 = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
         startDate.setText(dateFormat1.format(new Date()));
         endDate.setText(dateFormat1.format(new Date()));
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+        hour = c.get(Calendar.AM_PM);
+        minutes = c.get(Calendar.MINUTE);
     }
 
-    public void setOnClickListener() {
+    //点击事件
+    private void setOnClickListener() {
         back.setOnClickListener(clickListener);
         slip.setOnClickListener(clickListener);
         startDate.setOnClickListener(clickListener);
@@ -101,12 +128,10 @@ public class BusinessDataInfoActivity extends Activity {
                     }
                     break;
                 case R.id.start_date:
-                    Intent intent = new Intent(BusinessDataInfoActivity.this, DataTimePickerActivity.class);
-                    startActivityForResult(intent, 100);
+                    showdateAndTimePopup();
                     break;
                 case R.id.end_date:
-                    Intent intent1 = new Intent(BusinessDataInfoActivity.this, DataTimePickerActivity.class);
-                    startActivityForResult(intent1, 200);
+                    showdateAndTimePopup();
                     break;
                 case R.id.save_btn:
                     insertOaCalendar();
@@ -128,6 +153,118 @@ public class BusinessDataInfoActivity extends Activity {
         }
     };
 
+
+    //弹出选择日期时间选择框
+    public void showdateAndTimePopup() {
+        inflater = LayoutInflater.from(BusinessDataInfoActivity.this);
+        view = inflater.inflate(R.layout.popupwindow_business_datatimepicker, null);
+        popupWindow = new PopupWindow(view, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        //绑定控件ID
+        date = (DatePicker) view.findViewById(R.id.date);
+        time = (TimePicker) view.findViewById(R.id.time);
+        dateLayout = (LinearLayout) view.findViewById(R.id.date_layout);
+        timeLayout = (LinearLayout) view.findViewById(R.id.time_layout);
+        cancelRb = (RadioButton) view.findViewById(R.id.cancel_rb);
+        saveRb = (RadioButton) view.findViewById(R.id.save_rb);
+        //设置点击事件
+        date.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                dateChanged = true;
+                BusinessDataInfoActivity.this.year = year;
+                BusinessDataInfoActivity.this.month = monthOfYear;
+                BusinessDataInfoActivity.this.day = dayOfMonth;
+                dataString = year + "-" + (month+1) + "-" + day;
+            }
+        });
+
+        time.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                timeChanged = true;
+                BusinessDataInfoActivity.this.hour = hourOfDay;
+                BusinessDataInfoActivity.this.minutes = minute;
+                timeString = hour + ":" + minutes;
+            }
+        });
+        cancelRb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+        saveRb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(dateLayout.getVisibility() == View.VISIBLE){
+                    dateLayout.setVisibility(View.GONE);
+                    timeLayout.setVisibility(View.VISIBLE);
+                }else {
+                    if(dateChanged){   //日期改变了
+                        dateChanged = false;
+                        if(timeChanged){  //时间改变了
+                            try {
+                                timeChanged = false;
+                                startDate.setText(dateFormat.format(dateFormat1.parse(dataString+timeString)));
+                                Log.i("showdateAndTimePopup","日期改变，时间改变了"+startDate.getText());
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                        }else {
+                            try {
+                                StringBuilder sb = new StringBuilder(dateFormat1.format(new Date()));
+                                sb.replace(0,10,dataString);
+                                startDate.setText(dateFormat1.format(dateFormat1.parse(sb.toString())));
+                                Log.i("showdateAndTimePopup","日期改变，时间未改变进来了"+startDate.getText());
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }else {    //日期不改变
+                        if(timeChanged){  //时间改变了
+                            try {
+                                timeChanged = false;
+                                StringBuilder sb = new StringBuilder(dateFormat1.format(new Date()));
+                                sb.replace(11,16,timeString);
+                                startDate.setText(dateFormat1.format(dateFormat1.parse(sb.toString())));
+                                Log.i("showdateAndTimePopup","日期未改变，时间改变了"+sb.toString());
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                        }else {
+                            startDate.setText(dateFormat1.format(new Date()));
+                        }
+                    }
+                    popupWindow.dismiss();
+                }
+            }
+        });
+        popupWindow.update();
+        popupWindow.setBackgroundDrawable(getResources().getDrawable(R.color.white_transparent));
+        popupWindow.setAnimationStyle(R.style.camera);
+        popupWindow.showAtLocation(rootLinearlayout, Gravity.CENTER, 0, 0);
+        backgroundAlpha(0.6F);
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                backgroundAlpha(1.0F);
+                saveBtn.setClickable(true);
+            }
+        });
+    }
+
+    //设置背景透明度
+    public void backgroundAlpha(float bgAlpha) {
+        WindowManager.LayoutParams lp = BusinessDataInfoActivity.this.getWindow().getAttributes();
+        lp.alpha = bgAlpha; //0.0-1.0
+        if (bgAlpha == 1) {
+            BusinessDataInfoActivity.this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);//不移除该Flag的话,在有视频的页面上的视频会出现黑屏的bug
+        } else {
+            BusinessDataInfoActivity.this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);//此行代码主要是解决在华为手机上半透明效果无效的bug
+        }
+        BusinessDataInfoActivity.this.getWindow().setAttributes(lp);
+    }
+
     /**
      * 将信息保存到本地数据库OA日程表
      */
@@ -142,31 +279,6 @@ public class BusinessDataInfoActivity extends Activity {
         values.put("address", address.getText().toString().trim());
         values.put("details", detail.getText().toString().trim());
         db.insert("OaCalendar", null, values);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-                case 100:
-                    try {
-                        startDate.setText(dateFormat1.format(dateFormat1.parse(data.getStringExtra("date") + data.getStringExtra("time"))));
-                        Log.i("onActivityResult","获取的日期为："+data.getStringExtra("date"));
-                        Log.i("onActivityResult","获取的时间为："+data.getStringExtra("time"));
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case 200:
-                    try {
-                        endDate.setText(dateFormat1.format(dateFormat1.parse(data.getStringExtra("date") + data.getStringExtra("time"))));
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-            }
-        }
     }
 
     @Override
--- MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/mobile_business/DataTimePickerActivity.java
+++ MyApplicationSienke/app/src/main/java/com/example/administrator/thinker_soft/mobile_business/DataTimePickerActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TimePicker;
 
 import com.example.administrator.thinker_soft.R;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -19,28 +21,48 @@ import java.util.Locale;
  * Created by Administrator on 2017/6/27.
  */
 public class DataTimePickerActivity extends Activity {
-
-    private Button saveBtn;
     private DatePicker data;
     private TimePicker time;
-    private int year, month, day, hour, minute;
-    private String dataString, timeString, timeStringdefault;
+    private int year, month, day, hour, minutes;
+    private String dataString, timeString;
     private SimpleDateFormat dateFormat;
     private boolean dateChanged = false;
     private boolean timeChanged = false;
+    private LinearLayout dateLayout,timeLayout;
+    private RadioButton cancelRb,saveRb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_business_datatimepicker);
+        setContentView(R.layout.popupwindow_business_datatimepicker);
 
-        saveBtn = (Button) findViewById(R.id.save_btn);
+        bindView();
+        defaultSetting();
+        setOnClickListener();
+    }
+
+    //绑定控件
+    private void bindView() {
+        cancelRb = (RadioButton) findViewById(R.id.cancel_rb);
+        saveRb = (RadioButton) findViewById(R.id.save_rb);
         data = (DatePicker) findViewById(R.id.date);
         time = (TimePicker) findViewById(R.id.time);
+        dateLayout = (LinearLayout) findViewById(R.id.date_layout);
+        timeLayout = (LinearLayout) findViewById(R.id.time_layout);
+    }
 
-        defaultSetting();
+    private void defaultSetting() {
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
+        hour = calendar.get(Calendar.AM_PM);
+        minutes = calendar.get(Calendar.MINUTE);
+    }
 
-
+    //点击事件
+    private void setOnClickListener() {
         data.init(year, month, day, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -48,7 +70,7 @@ public class DataTimePickerActivity extends Activity {
                 DataTimePickerActivity.this.year = year;
                 DataTimePickerActivity.this.month = monthOfYear;
                 DataTimePickerActivity.this.day = dayOfMonth;
-                dataString = year + "-" + month + "-" + day;
+                dataString = year + "-" + (month+1) + "-" + day;
             }
         });
 
@@ -57,39 +79,54 @@ public class DataTimePickerActivity extends Activity {
             public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
                 timeChanged = true;
                 DataTimePickerActivity.this.hour = hourOfDay;
-                DataTimePickerActivity.this.minute = minute;
-                timeString = hour + ":" + minute;
+                DataTimePickerActivity.this.minutes = minute;
+                timeString = hour + ":" + minutes;
             }
         });
-        saveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                if (dateChanged) {
-                    intent.putExtra("date", dataString);
-                } else {
-                    intent.putExtra("date", dateFormat.format(new Date()));
-                }
-                if (timeChanged) {
-                    intent.putExtra("time", timeString);
-                } else {
-                    intent.putExtra("time", timeStringdefault);
-                }
-                setResult(RESULT_OK, intent);
-                finish();
-            }
-        });
+        cancelRb.setOnClickListener(clickListener);
+        saveRb.setOnClickListener(clickListener);
     }
 
-    private void defaultSetting() {
-        Calendar calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        hour = calendar.get(Calendar.HOUR);
-        minute = calendar.get(Calendar.MINUTE);
-        timeStringdefault = hour + ":" + minute;
-    }
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.cancel_rb:
+                    DataTimePickerActivity.this.finish();
+                    break;
+                case R.id.save_rb:
+                    if(dateLayout.getVisibility() == View.VISIBLE){
+                        dateLayout.setVisibility(View.GONE);
+                        timeLayout.setVisibility(View.VISIBLE);
+                    }else {
+                        Intent intent = new Intent();
+                        if(dateChanged){   //日期改变了
+                            if(timeChanged){  //时间改变了
+                                try {
+                                    intent.putExtra("dateAndTime", dateFormat.format(dateFormat.parse(dataString+" "+timeString)));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }else {
+                                StringBuffer sb = new StringBuffer(dateFormat.format(new Date()));
+                                sb.replace(0,10,dataString);
+                                intent.putExtra("dateAndTime", sb.toString());
+                            }
+                        }else {    //日期不改变
+                            if(timeChanged){  //时间改变了
+                                StringBuffer sb = new StringBuffer(dateFormat.format(new Date()));
+                                sb.replace(11,16,timeString);
+                                intent.putExtra("dateAndTime", sb.toString());
+                            }else {
+                                intent.putExtra("dateAndTime", dateFormat.format(new Date()));
+                            }
+                        }
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                    break;
+            }
+        }
+    };
 
 }
